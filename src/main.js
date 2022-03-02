ab694ffb1ad0ab00d6a0db1d777ee748c486e040
Suraj Yadav <ysuraj877@gmail.com>
2022-03-02 19:18:23 +0530

    migrate to es

---
+++ src/main.js
@@ -0,0 +1,84 @@
+import { isAbsolute, parse, dirname, basename, join, relative, extname } from "path"
+
+class Path {
+    /*
+        I am a filepath(files & directories)
+        You can use me by 
+            `Path.fromString(FILE_PATH)`
+            `new Path(FILE_PATH)`
+            `FILE_PATH_IN_STRING.asPath()`
+    */
+    constructor(pathString) {
+        this.path = pathString
+    }
+
+    static fromString(pathString) {
+        return new this(pathString)
+    }
+
+    isAbsolute() {
+        return isAbsolute(this.path)
+    }
+
+    isRelative() {
+        return !this.isAbsolute()
+    }
+
+    isDirectory() {
+        return parse(this.path).ext == ''
+    }
+
+    isFile() {
+        return !this.isDirectory()
+    }
+
+    directory() {
+        return Path.fromString(dirname(this.path))
+    }
+
+    file() {
+        if (this.isDirectory()) {
+            throw Error("Should be a file")
+        }
+
+        return Path.fromString(basename(this.path))
+    }
+
+    join(aPathOrString) {
+        // overloading aPath and String
+        if (aPathOrString instanceof Path) {
+            this.path = join(this.path, aPathOrString.path)
+        } else {
+            this.path = join(this.path, aPathOrString)
+        }
+        return this
+    }
+
+    parse() {
+        /*
+        Path.fromString('/home/jarusll/test.js').parse() -> {
+            root: '/',
+            dir: '/home/jarusll',
+            base: 'test.js',
+            name: 'test',
+            ext: 'js'
+        }
+        */
+        return parse(this.path)
+    }
+    relativeTo(aPathOrString) {
+        if (aPathOrString instanceof Path) {
+            return Path.fromString(relative(this.path, aPathOrString.path))
+        }
+        return Path.fromString(relative(this.path, aPathOrString))
+    }
+    extension() {
+        return extname(this.path)
+    }
+}
+
+export default Path
+
+String.prototype.asPath = function () {
+    return Path.fromString(this)
+}
---
+++ test/PathTest.js
@@ -0,0 +1,59 @@
+import {it} from "mocha"
+import Path from "../src/main.js"
+import {expect} from "chai"
+
+describe("Path", function(){
+    let testJsPath = new Path("/home/jarusll/test.js")
+    it("inits", function(){
+        expect(testJsPath).to.be.instanceOf(Path)
+    })
+    it("inits with correct path", function(){
+        expect(testJsPath.path).to.equal("/home/jarusll/test.js")
+    })
+    it("should give directory path", function(){
+        expect(testJsPath.directory()).to.be.instanceOf(Path)
+        expect(testJsPath.directory().path).to.equal('/home/jarusll')
+    })
+    it("should give file path", function(){
+        expect(testJsPath.file()).to.be.instanceOf(Path)
+        expect(testJsPath.file().path).to.equal('test.js')
+    })
+    it("should be true for absolute and false for relative", function(){
+        expect(testJsPath.isAbsolute()).to.be.true
+        expect(testJsPath.isRelative()).to.be.false
+    })
+    it("should be true for relative and false for absolute", function(){
+        expect(new Path("home/jarusll").isRelative()).to.be.true
+        expect(new Path("home/jarusll").isAbsolute()).to.be.false
+    })
+    it("should be true as file and false as directory", function(){
+        expect(testJsPath.isFile()).to.be.true
+        expect(testJsPath.isDirectory()).to.be.false
+    })
+    it("should join path when given as a Path instance", function(){
+        expect(testJsPath.directory().join(new Path("yolo.js")).path).to.equal("/home/jarusll/yolo.js")
+    })
+    it("should join path when given as a string", function(){
+        expect(testJsPath.directory().join("yolo.js").path).to.equal("/home/jarusll/yolo.js")
+    })
+    it("should parse given path", function(){
+        expect(testJsPath.parse()).to.deep.equal({
+            root: '/',
+            dir: '/home/jarusll',
+            base: 'test.js',
+            name: 'test',
+            ext: '.js'
+        })
+    })
+    it("should return relative path for path given as string", function(){
+        expect(testJsPath.relativeTo('/home/jarusll/abc/xyz').path).to.equal(Path.fromString('../abc/xyz').path)
+    })
+    it("should return relative path for path given as Path", function(){
+        expect(testJsPath.relativeTo(Path.fromString('/home/jarusll/abc/xyz')).path).to.equal(Path.fromString('../abc/xyz').path)
+    })
+    it("should return extension", function(){
+        expect(testJsPath.extension()).to.equal('.js')
+        expect(Path.fromString('abc.index.md').extension()).to.equal('.md')
+        expect(Path.fromString('.index').extension()).to.equal('')
+    })
+})
